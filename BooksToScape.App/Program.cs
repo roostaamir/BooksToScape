35e3990d0ba2798da8f3ca5e658597edb5162862
Amir Roosta <roostaamir73@gmail.com>
2024-03-02 19:32:49 +0100

    Add a small snippet to test document modification

--- BooksToScape.App/Program.cs
+++ BooksToScape.App/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using HtmlAgilityPack;
+using StreamWriter = System.IO.StreamWriter;
 
 const string scrapingUrl = "https://books.toscrape.com/";
 
@@ -17,3 +18,12 @@ var sideBarLinks = htmlDocument.DocumentNode
     .ToList();
 
 sideBarLinks.ForEach(Console.WriteLine);
+
+
+//Testing html Manipulation
+var headerElement = htmlDocument.DocumentNode
+    .SelectSingleNode("//*[@id=\"default\"]/div/div/div/div/div[1]/h1");
+headerElement.InnerHtml = "Other Title";
+
+await using var writer = new StreamWriter(Path.Combine(Environment.CurrentDirectory, "modified-doc.html"));
+htmlDocument.Save(writer);
