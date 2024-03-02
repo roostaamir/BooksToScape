9d3d2e7dd12115a248dbd6cb35c67e58305763be
Amir Roosta <roostaamir73@gmail.com>
2024-03-02 19:08:09 +0100

    Test a small scraping action to assess parser library
    - Adds a small piece of code the determine if HtmlAgility library is a good match for parsing html the way
      we want to work with it

--- BooksToScape.App/Program.cs
+++ BooksToScape.App/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using HtmlAgilityPack;
+
+const string scrapingUrl = "https://books.toscrape.com/";
+
+var client = new HttpClient();
+
+var responseString = await client.GetStringAsync(scrapingUrl);
+
+var htmlDocument = new HtmlDocument();
+htmlDocument.LoadHtml(responseString);
+
+var sideBarLinks = htmlDocument.DocumentNode
+    .SelectNodes("//*[@id=\"default\"]/div/div/div/aside/div[2]/ul/li/ul/li[*]/a")
+    .Select(x => x.Attributes["href"].Value)
+    .ToList();
+
+sideBarLinks.ForEach(Console.WriteLine);
