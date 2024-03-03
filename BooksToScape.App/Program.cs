d04e7c112a732916e899e83025147f2727e6e7d8
Amir Roosta <10849094+roostaamir@users.noreply.github.com>
2024-03-03 17:31:53 +0100

    Clean up program

--- BooksToScape.App/Program.cs
+++ BooksToScape.App/Program.cs
@@ -2,7 +2,6 @@
 
 using BooksToScape.App.Services;
 using HtmlAgilityPack;
-using StreamWriter = System.IO.StreamWriter;
 
 const string scrapingUrl = "https://books.toscrape.com/";
 
@@ -12,22 +11,6 @@ var responseString = await client.GetStringAsync(scrapingUrl);
 
 var htmlDocument = new HtmlDocument();
 htmlDocument.LoadHtml(responseString);
-
-var sideBarLinks = htmlDocument.DocumentNode
-    .SelectNodes("//*[@id=\"default\"]/div/div/div/aside/div[2]/ul/li/ul/li[*]/a")
-    .Select(x => x.Attributes["href"].Value)
-    .ToList();
-
-sideBarLinks.ForEach(Console.WriteLine);
-
-
-//Testing html Manipulation
-var headerElement = htmlDocument.DocumentNode
-    .SelectSingleNode("//*[@id=\"default\"]/div/div/div/div/div[1]/h1");
-headerElement.InnerHtml = "Other Title";
-
-await using var writer = new StreamWriter(Path.Combine(Environment.CurrentDirectory, "modified-doc.html"));
-htmlDocument.Save(writer);
 
 Console.WriteLine("Enter a path to Crawl");
 var path = Console.ReadLine();
